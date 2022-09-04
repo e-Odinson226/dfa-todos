d0f3934b387e4eb69f3a6af4b6af71666df394fb
zakaria <erfan.yekezare@gmail.com>
2022-09-04 12:18:23 +0430

    design TodoModel and add it to admin panel.

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TodoModel
 
-# Register your models here.
+
+class TodoAmdin(admin.ModelAdmin):
+    list_display = ("title", "body")
+
+
+admin.site.register(TodoModel, TodoAmdin)
