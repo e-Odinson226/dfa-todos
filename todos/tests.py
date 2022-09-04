4a1b61a56435bdecdf1416a289625c5fe00e71d0
zakaria <erfan.yekezare@gmail.com>
2022-09-04 12:30:43 +0430

    create tests on TodoModel

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,28 @@
+from urllib import response
 from django.test import TestCase
+from .models import TodoModel
 
-# Create your tests here.
+
+class TodoTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = TodoModel.objects.create(
+            title="testing title",
+            body="test todo body\
+            test todo body test todo body test todo body\
+            test todo body test todo body test todo body\
+            test todo body test todo body test todo body\
+            test todo body test todo body test todo body",
+        )
+
+    def test_TodoModel_data(self):
+        self.assertEqual(self.todo.title, "testing title")
+        self.assertEqual(
+            self.todo.body,
+            "test todo body\
+            test todo body test todo body test todo body\
+            test todo body test todo body test todo body\
+            test todo body test todo body test todo body\
+            test todo body test todo body test todo body",
+        )
+        self.assertEqual(str(self.todo), "testing title")
